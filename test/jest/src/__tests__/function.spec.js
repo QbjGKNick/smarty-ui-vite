784fad7ef77fd52895e2121b84e064db5a444982
jiangqb <jiangqb@citycloud.com.cn>
2022-09-07 17:02:45 +0800

    feat(vitest): add vitest

--- test/jest/src/__tests__/function.spec.js
+++
@@ -1,9 +0,0 @@
-// import events from '../src/events'
-// import fetch from '../src/fetch'
-// test('使用jest.spyOn()监控fetch.fetchPostsList被正常调用', async() => {
-//   expect.assertions(2)
-//   const spyFn = jest.spyOn(fetch, 'fetchPostsList')
-//   await events.getPostList()
-//   expect(spyFn).tpHaveBeenCalled()
-//   expect(spyFn).toHaveBeenCalledTimes(1)
-// })
